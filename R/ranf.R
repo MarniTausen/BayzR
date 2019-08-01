2b38775d829180e08fe4561619589d67e1af7e4d
ljanss <luc.janss@mbg.au.dk>
2019-08-01 09:14:24 +0200

    ranf.R changed argument to use V for kinship/similarity/var-covar matrix

--- R/ranf.R
+++ R/ranf.R
@@ -3,13 +3,13 @@
 #'  Sets a variable to a random variable in a linear mixed model.
 #'
 #' @param x     Variable vector which should be set to random in a model.
-#' @param cor   Correlation matrix to be included. Default uses eigen decomposition
+#' @param V   Correlation matrix to be included. Default uses eigen decomposition
 #' @param prior Prior information to be added.
 #'
 #' @return Random variable information.
 #' @export
 
-ranf <- function(x, cor=NULL, prior=NULL) {
+ranf <- function(x, V=NULL, prior=NULL) {
     x <- as.factor(x)
     if(!is.null(cor)) {
         EVdecomp <- eigen(cor)
