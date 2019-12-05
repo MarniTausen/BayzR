679a58e9ed89b47de0b107efaad25f9b0a3bbe8c
Marni Tausen <marni16ox@gmail.com>
2019-12-05 09:04:22 +0100

    Travis warnings not treated as errors

--- tests/testthat/testbayz.R
+++ tests/testthat/testbayz.R
@@ -21,7 +21,7 @@ test_that("Warning messages", {
 test_that("Wrong input", {
 
     my_data <- data.frame(x=1:20, y=20:1)
-    fit <- bayz(y ~ x, data=my_data, chain=c(1100,100,10))
+    fit <- bayz(y ~ x, data=my_data, chain=c(10000,100,10))
 
     expect_match(fit$Errors[1],
                  "Unknown wrapper function on data column")
@@ -37,17 +37,17 @@ test_that("Working run", {
 
     my_data <- data.frame(x=1:20, y=20:1)
 
-    expect_equal(bayz(y ~ fixf(x), data=my_data, chain=c(1100,100,10))$nError, 0)
-    expect_equal(bayz(y ~ ranf(x), data=my_data, chain=c(1100,100,10))$nError, 0)
+    expect_equal(bayz(y ~ fixf(x), data=my_data, chain=c(10000,100,10))$nError, 0)
+    expect_equal(bayz(y ~ ranf(x), data=my_data, chain=c(10000,100,10))$nError, 0)
 
-    capture.output(print(bayz(y ~ fixf(x), data=my_data, chain=c(1100,100,10))))
+    capture.output(print(bayz(y ~ fixf(x), data=my_data, chain=c(10000,100,10))))
 
 })
 
 test_that("Plotting", {
 
      my_data <- data.frame(x=1:20, y=20:1)
-     fit <- bayz(y ~ fixf(x), data=my_data, chain=c(1100,100,10))
+     fit <- bayz(y ~ fixf(x), data=my_data, chain=c(10000,100,10))
 
      plot(fit)
 
