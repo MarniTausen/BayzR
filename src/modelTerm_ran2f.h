6cf0fef04963bbaf4a516a63293911965c3b6929
ljanss <luc.janss@mbg.au.dk>
2020-01-14 11:16:32 +0100

    added modelTerm_ran2f to fit random interaction between two factors (code not finished yet)

---
+++ src/modelTerm_ran2f.h
@@ -0,0 +1,49 @@
+//
+//  modelTerm_rand2f.hpp
+//  rbayz
+//
+//  Created by Luc Janss on 14/01/2020.
+//
+
+#ifndef modelTerm_ran2f_h
+#define modelTerm_ran2f_h
+
+#include <Rcpp.h>
+#include "modelTerm_factor.h"
+#include "priorClasses.h"
+
+// Model-term for interaction between two random factors; there are also versions allowing
+// for one or two covariance-structures.
+
+class modelTerm_ran2f : public modelTerm_factor {
+
+public:
+
+   modelTerm_ran2f(Rcpp::DataFrame &d, size_t col)  : modelTerm_factor(d, col) {
+      hpar.resize(1,1);
+      hparName = "var." + parName;
+   }
+
+   ~modelTerm_ran2f() {
+   }
+
+   void sample() {
+      resid_decorrect();
+      collect_lhs_rhs();
+      for(size_t k=0; k<par.size(); k++) {
+         // random effect: add 1/hpar[0] in lhs
+         par[k] = R::rnorm( (rhs[k]/(lhs[k]+(1/hpar[0]))), sqrt(1.0/(lhs[k]+(1/hpar[0]))));
+      }
+      resid_correct();
+      // update hyper-par (variance) using SSQ of random effects
+      double ssq=0.0;
+      for(size_t k=0; k<par.size(); k++)
+         ssq += par[k]*par[k];
+      hpar[0] = gprior.samplevar(ssq,par.size());
+   }
+
+private:
+
+};
+
+#endif /* modelTerm_ran2f_h */
