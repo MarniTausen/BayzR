f04c6c9859a91fc0e66ce4fb2dc54365cc644bb9
ljanss <luc.janss@mbg.au.dk>
2020-01-17 10:48:13 +0100

    temporarily removed some parts of testbayz.R (and restored again)

--- tests/testthat/testbayz.R
+++ tests/testthat/testbayz.R
@@ -9,28 +9,28 @@ test_that("Missing inputs", {
 
 })
 
-#test_that("Warning messages", {
-#
-#    my_data <- data.frame(x=1:20, y=20:1)
-#
-#    expect_warning(bayz(y ~ x, data=my_data))
-#    #expect_silent(bayz(y ~ x, data=my_data, chain=c(10000, 100, 10)))
-#
-#})
+test_that("Warning messages", {
 
-#test_that("Wrong input", {
-#
-#    my_data <- data.frame(x=1:20, y=20:1)
-#    fit <- bayz(y ~ x, data=my_data, chain=c(10000,100,10))
-#
-#    expect_match(fit$Errors[1],
-#                 "Unknown wrapper function on data column")
-#    expect_match(fit$Errors[2],
-#                 "Bayz terminates after model building")
-#    expect_equal(fit$nError, 2)
-#
-#    capture.output(print(fit))
-#})
+    my_data <- data.frame(x=1:20, y=20:1)
+
+    expect_warning(bayz(y ~ x, data=my_data))
+    #expect_silent(bayz(y ~ x, data=my_data, chain=c(10000, 100, 10)))
+
+})
+
+test_that("Wrong input", {
+
+    my_data <- data.frame(x=1:20, y=20:1)
+    fit <- bayz(y ~ x, data=my_data, chain=c(10000,100,10))
+
+    expect_match(fit$Errors[1],
+                 "Unknown wrapper function on data column")
+    expect_match(fit$Errors[2],
+                 "Bayz terminates after model building")
+    expect_equal(fit$nError, 2)
+
+    capture.output(print(fit))
+})
 
 
 test_that("Working run", {
