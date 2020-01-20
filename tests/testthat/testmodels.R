5e6f77b2ff68ec7552ede5e4565ba489f41dee25
Marni Tausen <marni16ox@gmail.com>
2020-01-20 09:52:47 +0100

    Fixed tests and included freg test

--- tests/testthat/testmodels.R
+++ tests/testthat/testmodels.R
@@ -138,56 +138,56 @@ test_that("Genotype and Environment interactions effects model", {
 
 })
 
-# test_that("Genotype and Environment continous", {
-#
-#     n_samples <- 5000
-#
-#     Genotype_names <- c("AA", "AB", "AC", "AD",
-#                         "BA", "BB", "BC", "BD",
-#                         "CA", "CB", "CC", "CD",
-#                         "DA", "DB", "DC", "DD")
-#
-#     Geno_effects <- c(200, 150, 70, 30,
-#                       125, 175, 40, 75,
-#                       25, -40, -100, 10,
-#                       80, -25, -80, 125)
-#     names(Geno_effects) <- Genotype_names
-#
-#     Genotypes <- Genotype_names[runif(n_samples, 1, length(Genotype_names)+1)]
-#
-#     Env_1_model <- function(x) -0.25*x^2+15*x-100
-#
-#     Env_1 <- rnorm(n_samples, mean=15, sd=7)
-#
-#     Env_2_model <- function(x) -7*x^2+75*x-100
-#
-#     Env_2 <- rnorm(n_samples, mean=7, sd=2)
-#
-#     intercept <- 300
-#     noise <- rnorm(n_samples, mean=0, sd=40)
-#
-#     slope <- 0.7
-#
-#     Response <- intercept + sapply(Genotypes, function(x) Geno_effects[x]) +
-#     2*sapply(Env_1, Env_1_model) +
-#     2*sapply(Env_2, Env_2_model)
-#     names(Response) <- NULL
-#
-#     print("Continous variables!")
-#     fit <- bayz(Response ~ ranf(Genotypes), chain=c(3e4,100,10))
-#
-#     print(summary(fit))
-#
-#     expect_gte(summary(fit)$Heritability$Heritability[2], 0.15)
-#     expect_lte(summary(fit)$Heritability$Heritability[2], 0.35)
-#
-#     expect_error(bayz(Response ~ ranf(Genotypes) + freg(Env_1) + freg(Env_2), chain=c(3e4,100,10)))
-#
-#     print(summary(fit))
-#
-#     #expect_gte(summary(fit)$Heritability$Heritability[2], 0.95)
-#
-# })
+test_that("Genotype and Environment continous", {
+
+    n_samples <- 5000
+
+    Genotype_names <- c("AA", "AB", "AC", "AD",
+                        "BA", "BB", "BC", "BD",
+                        "CA", "CB", "CC", "CD",
+                        "DA", "DB", "DC", "DD")
+
+    Geno_effects <- c(200, 150, 70, 30,
+                      125, 175, 40, 75,
+                      25, -40, -100, 10,
+                      80, -25, -80, 125)
+    names(Geno_effects) <- Genotype_names
+
+    Genotypes <- Genotype_names[runif(n_samples, 1, length(Genotype_names)+1)]
+
+    Env_1_model <- function(x) -0.25*x^2+15*x-100
+
+    Env_1 <- rnorm(n_samples, mean=15, sd=7)
+
+    Env_2_model <- function(x) -7*x^2+75*x-100
+
+    Env_2 <- rnorm(n_samples, mean=7, sd=2)
+
+    intercept <- 300
+    noise <- rnorm(n_samples, mean=0, sd=40)
+
+    slope <- 0.7
+
+    Response <- intercept + sapply(Genotypes, function(x) Geno_effects[x]) +
+    2*sapply(Env_1, Env_1_model) +
+    2*sapply(Env_2, Env_2_model)
+    names(Response) <- NULL
+
+    print("Continous variables!")
+    fit <- bayz(Response ~ ranf(Genotypes), chain=c(3e4,100,10))
+
+    print(summary(fit))
+
+    expect_gte(summary(fit)$Heritability$Heritability[2], 0.15)
+    expect_lte(summary(fit)$Heritability$Heritability[2], 0.4)
+
+    fit <- bayz(Response ~ ranf(Genotypes) + freg(Env_1) + freg(Env_2), chain=c(3e4,100,10))
+
+    print(summary(fit))
+
+    expect_gte(summary(fit)$Heritability$Heritability[2], 0.5)
+
+})
 
 test_that("GRM tests", {
 
@@ -231,12 +231,14 @@ test_that("GRM tests", {
 
     print(summary(fit))
 
-    expect_equal(is.na(summary(fit)$Heritability$Heritability[2]), TRUE)
+    expect_gte(summary(fit)$Heritability$Heritability[2], 0.9)
+    #expect_equal(is.na(summary(fit)$Heritability$Heritability[2]), TRUE)
 
     fit <- bayz(Response ~ ranf(Genotypes, V=GRM) + ran2f(Genotypes, Genotypes, V1=GRM, V2=GRM) + fixf(Env_1) + fixf(Env_2), chain=c(3e4,100,10))
 
     print(summary(fit))
 
-    expect_equal(is.na(summary(fit)$Heritability$Heritability[2]), TRUE)
+    expect_gte(summary(fit)$Heritability$Heritability[2], 0.9)
+    #expect_equal(is.na(summary(fit)$Heritability$Heritability[2]), TRUE)
 
 })
