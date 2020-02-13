7e28288283fde4ff06ed246a0dd45ce7583156c2
Luc Janss <au236902@uni.au.dk>
2020-02-13 14:01:23 +0100

    Updated ranf and ran2f interfaces with rrankpct argument (other coding not yet finished)
    Updated ranf and ran2f interfaces with rrankpct argument (other coding not yet finished)

--- R/ran2f.R
+++ R/ran2f.R
@@ -6,12 +6,13 @@
 #' @param x2    Second variable vector (a factor) to build two-factor interaction
 #' @param V1    Similarity/var-covar matrix to be included on first variable. Uses/stores the eigen decomposition of V1.
 #' @param V2    Similarity/var-covar matrix to be included on second variable. Uses/stores the eigen decomposition of V2.
+#' @param rrankpct Reduced Rank threshold for including eigenvectors in percent (default 99%)
 #' @param prior Prior information for the variance component.
 #'
 #' @return The first factor data is returned, the second factor and V1 and V2 eigenvectors (when available) are stored as attributes.
 #' @export
 
-ran2f <- function(x1, x2, V1=NULL, V2=NULL, prior=NULL) {
+ran2f <- function(x1, x2, V1=NULL, V2=NULL, rrankpct=99, prior=NULL) {
     x1 <- as.factor(x1)
     x2 <- as.factor(x2)
     if(!is.null(V1)) {
@@ -28,5 +29,6 @@ ran2f <- function(x1, x2, V1=NULL, V2=NULL, prior=NULL) {
     }
     attr(x1, "factor2") <- x2
     if (!is.null(prior)) attr(x1,"prior") <- prior
+    attr(x1,"rrankpct") <- rrankpct
     return(x1)
 }
--- R/ranf.R
+++ R/ranf.R
@@ -4,12 +4,13 @@
 #'
 #' @param x     Variable vector which should be set to random in a model.
 #' @param V     Similarity/var-covar matrix to be included so that x~N(0,Vsigma^2). Uses eigen decomposition
+#' @param rrankpct Reduced Rank threshold for including eigenvectors in percent (default 99%)
 #' @param prior Prior information to be added.
 #'
 #' @return Random variable information.
 #' @export
 
-ranf <- function(x, V=NULL, prior=NULL) {
+ranf <- function(x, V=NULL, rrankpct=99, prior=NULL) {
     x <- as.factor(x)
     if(!is.null(V)) {
         EVdecomp <- eigen(V)
@@ -18,5 +19,6 @@ ranf <- function(x, V=NULL, prior=NULL) {
         attr(x, "evectors") <- EVdecomp$vectors
     }
     if (!is.null(prior)) attr(x,"prior") <- prior
+    attr(x,"rrankpct") <- rrankpct
     return(x)
 }
