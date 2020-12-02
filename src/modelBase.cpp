b657ba9e97b24c42c4d037a5caf944a45301b8a7
Luc Janss <luc.janss@qgg.au.dk>
2020-12-02 11:35:15 +0100

    put code from modelBase.h in a cpp file

---
+++ src/modelBase.cpp
@@ -0,0 +1,34 @@
+#include "modelBase.h"
+
+// standard modelBase constructor
+modelBase::modelBase(std::string modelTerm, Rcpp::DataFrame &d, simpleMatrix &e, size_t resp)
+                 : gprior(modelTerm)
+   {
+      resid = e.data[2*resp];
+      residPrec = e.data[2*resp+1];
+      Nresid = e.nrow;
+      parName = getVarNames(modelTerm);
+      size_t pos;
+      // when making real hierarchical models, here the higher model could be isolated
+      // and may be inserted as a new model inside the current model object?
+      if( (pos=parName.find(';')) != std::string::npos) {
+         hasIndexVariable=TRUE;
+         parName[pos]=':';
+      }
+      varNames = splitString(parName,':');
+      for(size_t i=0; i<varNames.size(); i++) {
+         if (varNames[i]=="1" || varNames[i]=="0") {
+            varObjects.push_back(R_NilValue);
+            varType.push_back(0);
+         }
+         else {
+            varObjects.push_back(getVariableObject(d,varNames[i]));
+            if(varObjects.back() != R_NilValue)
+               varType.push_back(getVariableType(varObjects.back()));
+            else {
+               throw generalRbayzError("Variable name not found: "+varNames[i]);
+            }
+         }
+      }
+   }
+
