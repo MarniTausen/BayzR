88d10243712a149dee840b279e1f951efb7bf6a3
Luc Janss <luc.janss@qgg.au.dk>
2020-12-09 14:39:06 +0100

    Started making a modelBVS derived from modelRreg

--- R/bayz.R
+++ R/bayz.R
@@ -7,56 +7,57 @@
 #' explanatory (right-hand-side) terms wrapped by a function to specify how to fit
 #' the explanatory variables in the model. This may look like Yield ~ fx(Year) + rn(Variety)
 #' to fit Yield with Year as a fixed factor and Variety as a random factor. 
-#' The equivalent lme4 model would
-#' be Yield ~ factor(Year) + (1|Variety). The model-functions have extensions to specify
-#' variances, priors, and sometimes fit options. The list of
-#' model functions currently available is: fx() to fit a fix factor or interaction of factors,
-#' rn() to fit a random factor or interaction of factors, rg() to fit fixed regressions or
-#' nested regressions (regressions within each level of a factor), and rr() to fit random
-#' regressions including random slopes and fit of large sets of covariates by random regression
-#' or Bayesian shrinkage models. [refer to functions help]. 
+#' The equivalent lme4 model would be Yield ~ factor(Year) + (1|Variety). 
+#' The list of model functions currently available is:
+#' fx() (fixed factors), rn() (random factors), rg()
+#' (fixed regressions), and rr() (random regressions). 
+#' The model functions allow to specify interactions of variables, hierarchies, use of
+#' matrices as input data, complex covariance structures, and prior distributions can 
+#' be changed to modify standard mixed model in Bayesian shrinkage models.
 #'
 #' Interactions between fators are specified using the colon, for instance by writing for a 
-#' fixed factor fit with Yield-Location interaction: Yield ~ fx(Year:Location). Bayz does
+#' fixed Year-Location interaction fx(Year:Location). Bayz does
 #' NOT support automatic expansion with main effects by using the 'star' (Year*Location) or
 #' 'forward slash' (Year/Location) syntaxes, hence bayz requires to manually add the desired
-#' main effects in the model. Interactions between factors can be specified to any degree.
-#' The 'star' syntax, however, can be used to indicate interaction
-#' between covariates, like Yield ~ rg(TempSum*Precip), where it is simply interpreted as
+#' main effects in the model (but 
+#' note that bayz uses the 'forward slash' to specify hierarchical models).
+#' Interactions between factors can be specified to any degree.
+#' The 'star' syntax can be used to indicate interaction
+#' between covariates, like rg(TempSum*Precip), where it is simply interpreted as
 #' multiplication. 
-#' Interaction between a covariate and a factor is specified using the `pipe' or vertical bar
-#' as in lme4 models. It can be used to specify fixed nested regressions for instance Yield ~ 
+#' Interaction between a covariate and a factor is specified using the 'pipe'
+#' character as in lme4 models. It can be used to specify fixed nested regressions as 
 #' rg(TempSum|Year:Location) to specify regressions on TempSum within each Year-Location, or
-#' to speficy random slope models in rr(), for instance Yield ~ rr(TempSum|Variety). 
-#' Note that bayz uses the 'forward slash' to specify hierarchical models.
+#' to speficy random slope models in rr(), for instance rr(TempSum|Variety). 
 #'
-#' The bayz call has a data argument to specify an input data frame. Any variable specified
-#' in the model will be searched first in the input data-frame 
-#' and, if not found in the data frame, bayz will search the R environment. Typical use is
-#' to place response variables, single covariates and factors in the input data frame,
-#' but large sets of covariates and variance matrices are objects in the R
-#' environment. Objects outside the data frame must have row-names attached to link
-#' to a subject/sample ID in the data frame. For random effect with a covariance
-#' structure, this is used as
-#' Yield ~ rn(Variety, V=kinmat), where 'kinmat' is a (proportional) variance-covariance
-#' structure (kinship matrix), which then has row-names to match to Variety levels.
-#' Fitting of a set of covariates as random regressions or with Bayesian
-#' shrinkage models, is specified with a hierachical model specification as Yield ~
+#' The bayz call has a data argument to specify an input data-frame (the "main data")
+#' to contain model variables,
+#' but bayz will also search the R environment if variables are not found in the main data.
+#' Typically, input in the form of matrices such as large sets of covariates, proportional
+#' variance-covariance / correlation / kernel / similarity matrices used in variance models,
+#' as well as data for hierarchical models, are not in the main data.
+#' Matrices / kernels should be prepared with row-names to match a variable
+#' in the data. For matrices / kernels used in variance models, the link is straightforward, 
+#' for instance in rn(Variety:Location, V=KG*KE), KG and KE can be a genetic and environmental
+#' kinship / kernel matrix, respectively, and KG should have row-names matching Variety levels,
+#' while KE should have row-names matching Location levels. 
+#' To fit a large set of covariates, the match is specified using a hierachical specification
 #' rr(Variety/Metabolites), where Metabolites is then a matrix of covariates which must have
-#' row-names matching Variety levels. 
+#' row-names matching Variety levels. In both cases, such kernel or covariate matrices
+#' must have unique levels, but the main data may have repeated levels and in different order.
+#' If one has repeated metabolite data on each Variety, for instance at multiple time-points,
+#' then consider that Variety is not the appropriate link to the data, but Variety:Time is.
 #'
 #' For random effects a variance-covariance structure can be specified using a
-#' V= option within the model-term function, for example Yield ~ rn(Variety, V=kinmat).
-#' When the fit is for an interaction of factors, also the variance specification should
-#' expand to include one term for each variable, separated by stars. The variance structure
-#' is then built up from a list of given (proportional) variance-covariance or correlation
-#' structures and from predefined acronyms IDEN, DIAG and VCOV to imply variance-covariance
-#' structures with unknown (to be estimated from the data) parameters. 
-#' for instance a fit of Location-Variety interaction effects could be 
-#' Yield ~ rn(Location:Variety, V=loccor*kinmat), where 'loccor' is a matrix with known
-#' correlations between locations. As explained above, 'loccor' should have row-names
-#' matching the Locations in the data. Apart from known correlation-structures also 
-#' special 
+#' V= option within the model-term function, for example rn(Variety, V=KG).
+#' When the fit is for an interaction of factors, the variance specification should
+#' expand to include one term for each variable, separated by stars (which should
+#' be read as Kronecker products). The variance structure
+#' is then built up from a combination of given (proportional) variance-covariance / correlation /
+#' kernel matrices and predefined acronyms IDEN, DIAG and VCOV that indicate parameterized
+#' matrices (with parameters to be estimated from the data). 
+#' Alternatively, the variance structure can be specified as a linear model using V=~, 
+#' which is interpreted as a use of a log-linear model for the variances. 
 #'
 #' Bayesian shrinkage models ...
 #'
---
+++ src/modelBVS.h
@@ -0,0 +1,47 @@
+//
+//  BayzR --- modelRreg.hpp
+//
+//  Computational class to to model random regressions.
+//  This used all methods from modelMatrix, only some parameter names need to be set.
+//
+//  Created by Luc Janss on 03/08/2018.
+//
+
+#ifndef modelRreg_h
+#define modelRreg_h
+
+#include <Rcpp.h>
+#include "modelMatrix.h"
+#include "dataMatrix.h"
+
+class modelRreg : public modelMatrix {
+
+public:
+
+   modelRreg(std::string modelTerm, Rcpp::DataFrame &d, simpleMatrix &e, size_t resp)
+         : modelMatrix(modelTerm, d, e, resp)
+   {
+      // Note: modelMatrix checks and sets up factor and matrix, Rreg is an implementation
+      // of modelMatrix that only needs to add modeling of variances
+      par = regcoeff;    // it sould be a shallow copy, par.data must point to the
+                         // same memory as regcoeff.data
+      hpar.initWith(1,1.0l);
+      hparName = "var." + parName;
+   }
+
+   ~modelRreg() {
+   }
+   
+   void sample() {
+      update_regressions();
+      // update hyper-par (variance) using SSQ of random effects
+      double ssq=0.0;
+      for(size_t k=0; k< M->ncol; k++)
+         ssq += par[k]*par[k]/M->weights[k];
+      hpar[0] = gprior.samplevar(ssq, M->ncol);
+   }
+
+
+};
+
+#endif /* modelRreg */
