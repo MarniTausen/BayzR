6e65546141070e44ef30f20e85651a23372fb678
Luc Janss <luc.janss@qgg.au.dk>
2021-05-06 20:53:10 +0200

    Update kernelMatrix.h
    Copied most from old ran2f to new class kernelMatrix

--- src/kernelMatrix.h
+++ src/kernelMatrix.h
@@ -1,5 +1,7 @@
 //  kernelMatrix.h
-//  Storage of kernel as eigen-decomposition. Derives from simpleMatrix.
+//  Storage of kernel as eigen-decomposition.
+//  Derives from simpleMatrix using the simpleMatrix() contructor, so that
+//  no matrix data is stored yet from the parent constructor.
 //
 //  Created by Luc Janss on 06/05/2021.
 //
@@ -22,65 +24,46 @@ int addMatrixNames(std::vector<std::string> & names, Rcpp::NumericMatrix & mat, 
 class kernelMatrix : public simpleMatrix {
 
 public:
-   kernelMatrix(Rcpp::RObject col, std::string & name) : simpleMatrix(col) {
-      // constuctor of simpleMatrix has already read and stored the input matrix, but
-      // need to reform it to store the eigen-decomposition.
-      Rcpp::NumericMatrix tempdata = Rcpp::as<Rcpp::NumericMatrix>(col);
-      if (addMatrixNames(rownames, tempdata, 1) >0) {
-         throw generalRbayzError("No rownames on matrix " + name + "\n");
-      }
-      addMatrixNames(colnames, tempdata, 2); // colnames are allowed to be missing
-      double * datacol;
-      size_t i,j;
-      double sum;
-      for(i=0; i<ncol; i++) {
-         datacol = data[i];
-         sum=0.0l;
-         for(j=0; j<nrow; j++) sum+=datacol[j];
-         for(j=0; j<nrow; j++) datacol[j] -= sum;
-      }
-      /*  this part needs to be reviewed, it needs to move to variance-input
-      if (col.hasAttribute("evectors")) {
-         throw(generalRbayzError("Evector input as matrix data temporarily disables\n"));
-         double rrankpct;
-         data = Rcpp::as<Rcpp::NumericMatrix>(col.attr("evectors"));
-         std::vector<std::string> att = data.attributeNames();
-         Rcpp::Rcout << "Handling evector matrix with attributes: ";
-         for(size_t i=0; i<att.size(); i++) Rcpp::Rcout << " " << att[i];
-         Rcpp::Rcout << "\n";
-         if (col.hasAttribute("evalues")) {
-            weights = Rcpp::as<Rcpp::NumericVector>(col.attr("evalues"));
-            // the weigts should actually be the inverse eigen-values, but it is dangerous
-            // to start making the inverses already here because some eigenvalues can be zero ...
-         }
-         else
-            throw(generalRbayzError("Eigen-decomp storage corrupted - no evalues"));
-         if(data.nrow() != data.ncol())
-            throw(generalRbayzError("Eigen-decomp has non-square e-vectors matrix"));
-         if(weights.size() != data.ncol())
-            throw(generalRbayzError("Eigen-decomp vectors and values size do not match"));
-         if(col.hasAttribute("rrankpct"))
-            rrankpct = col.attr("rrankpct");
-         else
-            rrankpct = 99;
-         double sumeval = 0.0l;
-         for (size_t i = 0; i<weights.size() && weights[i] > 0; i++) sumeval += weights[i];
-         double eval_cutoff = rrankpct * sumeval / 100.0l;
-         nColUsed = 0;
-         sumeval = 0.0l;
-         while (sumeval < eval_cutoff) sumeval += weights[nColUsed++];
-         // this message can be moved to the modelTerm routine; and it must be different for ran2f_2cor.
-         Rcpp::Rcout << "In ranf with V rrankpct=" << rrankpct << " uses " << nColUsed << " eigenvectors\n";
-         getMatrixNames(labels, data);  // can be moved outside if-else when other matrix forms are also coded
-      }
-      else  {
-       */
+
+   kernelMatrix(Rcpp::RObject col, std::string & name) : simpleMatrix(), weights() {
+      kerneldata = Rcpp::as<Rcpp::NumericMatrix>(col);
+   	Rcpp::Function eig("eigen");
+	   Rcpp::List eigdecomp;
+   	try {
+	   	eigdecomp = eig(kerneldata);
+   	}
+   	catch(std::exception &err) {
+         throw(generalRbayzError("An error occurred running eigen(): "+err.what()));
+   	}
+   	Rcpp::NumericVector eigvalues = eigdecomp["values"];
+	   Rcpp::NumericMatrix eigvectors = eigdecomp["vectors"];
+      // Determine how many evectors to keep
+      double rrankpct;
+      size_t nColUsed = 0;
+      if(col.hasAttribute("rrankpct"))
+         rrankpct = col.attr("rrankpct");
+      else
+         rrankpct = 99;
+      double sumeval = 0.0l;                   // only summing the positive ones!
+      for (size_t i = 0; i<eigvalues.size() && eigvalues[i] > 0; i++) sumeval += eigvalues[i];
+      double eval_cutoff = rrankpct * sumeval / 100.0l;
+      sumeval = 0.0l;
+      while (sumeval < eval_cutoff) sumeval += eigvalues[nColUsed++];
+      // this message can be moved to the modelTerm routine; and it must be different for ran2f_2cor.
+      Rcpp::Rcout << "For kernel " << name << " with rrankpct=" << rrankpct << " using "
+                  << nColUsed << " eigenvectors\n";
+      this->initWith(eigvectors, nColUsed);
+      weights.initWith(eigvalues, nColUsed);
+      addMatrixNames(rownames, kerneldata, 1);
+      // if want to add colnames, it can be evec1, evec2, etc. The original colnames
+      // on the kernel (if present) have become meaningless after the eigen-decomp.
    }
 
    ~kernelMatrix() {
    }
 
    std::vector<std::string> rownames, colnames;
+   simpleDblVector weights;
    
 };
 
