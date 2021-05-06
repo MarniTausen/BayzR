70a69096d0dddb606e6166806c71bc73b1224bbd
Luc Janss <luc.janss@qgg.au.dk>
2021-05-06 10:52:10 +0200

    Update dataMatrix.h
    Fixed error in centering and cleaned up comments

--- src/dataMatrix.h
+++ src/dataMatrix.h
@@ -1,9 +1,9 @@
 //
 //  dataMatrix.h
-//  Class for storage of matrix input data (matrix of covariates or similarity / kernel matrix).
-//  This is a slightly specialised (derived) version of simpleMatrix with row and column names,
-//  row-names are mandatory (missing rownames throws error), colnames are optional.
-//  Note: now also doing centering of columns, that's not needed for kernels!
+//  Class for storage of matrix of covariates (e.g. input in rr() model).
+//  Derives from simpleMatrix and using simpleMatrix(Robject) constructor so that the
+//  input is already stored in parent class constructor.
+//  Here add row and optionally column names and center the input data.
 //
 //  Created by Luc Janss on 05/03/2020.
 //
@@ -13,11 +13,9 @@
 
 #include <stdio.h>
 #include <Rcpp.h>
-#include "rbayzExceptions.h"
 #include <string>
-#include <vector>
+#include "rbayzExceptions.h"
 #include "simpleMatrix.h"
-#include "simpleVector.h"
 //#include "nameTools.h"  // strange, including nameTools.h does not work to make
                           // addMatrixNames available here ...
 
@@ -27,13 +25,13 @@ class dataMatrix : public simpleMatrix {
 
 public:
    dataMatrix(Rcpp::RObject col, std::string & name) : simpleMatrix(col) {
-      // constuctor of simpleMatrix has already done the matrix data, here add
-      // weights and labels, and column-center the covariates.
+      // I need to temporarily redo the conversion of the input Robject to
+      // Rcpp::NumericMatrix to retrieve row and col names.
       Rcpp::NumericMatrix tempdata = Rcpp::as<Rcpp::NumericMatrix>(col);
       if (addMatrixNames(rownames, tempdata, 1) >0) {
          throw generalRbayzError("No rownames on matrix " + name + "\n");
       }
-      addMatrixNames(colnames, tempdata, 2); // colnames are allowed to be missing
+      addMatrixNames(colnames, tempdata, 2); // no throw here, colnames are optional
       double * datacol;
       size_t i,j;
       double sum;
@@ -41,44 +39,9 @@ public:
          datacol = data[i];
          sum=0.0l;
          for(j=0; j<nrow; j++) sum+=datacol[j];
+         sum =/ double(nrow);
          for(j=0; j<nrow; j++) datacol[j] -= sum;
       }
-      /*  this part needs to be reviewed, it needs to move to variance-input
-      if (col.hasAttribute("evectors")) {
-         throw(generalRbayzError("Evector input as matrix data temporarily disables\n"));
-         double rrankpct;
-         data = Rcpp::as<Rcpp::NumericMatrix>(col.attr("evectors"));
-         std::vector<std::string> att = data.attributeNames();
-         Rcpp::Rcout << "Handling evector matrix with attributes: ";
-         for(size_t i=0; i<att.size(); i++) Rcpp::Rcout << " " << att[i];
-         Rcpp::Rcout << "\n";
-         if (col.hasAttribute("evalues")) {
-            weights = Rcpp::as<Rcpp::NumericVector>(col.attr("evalues"));
-            // the weigts should actually be the inverse eigen-values, but it is dangerous
-            // to start making the inverses already here because some eigenvalues can be zero ...
-         }
-         else
-            throw(generalRbayzError("Eigen-decomp storage corrupted - no evalues"));
-         if(data.nrow() != data.ncol())
-            throw(generalRbayzError("Eigen-decomp has non-square e-vectors matrix"));
-         if(weights.size() != data.ncol())
-            throw(generalRbayzError("Eigen-decomp vectors and values size do not match"));
-         if(col.hasAttribute("rrankpct"))
-            rrankpct = col.attr("rrankpct");
-         else
-            rrankpct = 99;
-         double sumeval = 0.0l;
-         for (size_t i = 0; i<weights.size() && weights[i] > 0; i++) sumeval += weights[i];
-         double eval_cutoff = rrankpct * sumeval / 100.0l;
-         nColUsed = 0;
-         sumeval = 0.0l;
-         while (sumeval < eval_cutoff) sumeval += weights[nColUsed++];
-         // this message can be moved to the modelTerm routine; and it must be different for ran2f_2cor.
-         Rcpp::Rcout << "In ranf with V rrankpct=" << rrankpct << " uses " << nColUsed << " eigenvectors\n";
-         getMatrixNames(labels, data);  // can be moved outside if-else when other matrix forms are also coded
-      }
-      else  {
-       */
    }
 
    ~dataMatrix() {
