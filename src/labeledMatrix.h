74d7f7c13826f7ee7f29c1c01bc19c6bccbd8808
Luc Janss <33286944+ljanss@users.noreply.github.com>
2021-05-10 10:04:55 +0200

    Create labeledMatrix.h
    There was need for a simpleMatrix derivation with row and colnames

---
+++ src/labeledMatrix.h
@@ -0,0 +1,41 @@
+//  labeledMatrix.h
+//  A simpleMatrix with 'labels' (row and column names).
+//  Rownames are required, colnames are optional.
+//
+//  Created by Luc Janss on 10/05/2021.
+//
+
+#ifndef labeledMatrix_h
+#define labeledMatrix_h
+
+#include <Rcpp.h>
+#include <vector>
+#include <string>
+#include "rbayzExceptions.h"
+#include "simpleMatrix.h"
+//#include "nameTools.h"  // strange, including nameTools.h does not work to make
+                          // addMatrixNames available here ...
+
+int addMatrixNames(std::vector<std::string> & names, Rcpp::NumericMatrix & mat, int dim);
+
+class labeledMatrix : public simpleMatrix {
+
+public:
+   labeledMatrix(Rcpp::RObject col, std::string & name) : simpleMatrix(col) {
+      // I need to temporarily redo the conversion of the input Robject to
+      // Rcpp::NumericMatrix to retrieve row and col names.
+      Rcpp::NumericMatrix tempdata = Rcpp::as<Rcpp::NumericMatrix>(col);
+      if (addMatrixNames(rownames, tempdata, 1) >0) {
+         throw generalRbayzError("No rownames on matrix " + name + "\n");
+      }
+      addMatrixNames(colnames, tempdata, 2); // no throw here, colnames are optional
+   }
+
+   ~labeledMatrix() {
+   }
+
+   std::vector<std::string> rownames, colnames;
+   
+};
+
+#endif /* labeledMatrix_h */
