1d6c598390855e03f7a4afb8a7353cccad4fa76a
Luc Janss <luc.janss@qgg.au.dk>
2021-05-10 09:09:48 +0200

    Added addKernel() to make kronecker product

--- src/kernelMatrix.h
+++ src/kernelMatrix.h
@@ -38,12 +38,8 @@ public:
    	Rcpp::NumericVector eigvalues = eigdecomp["values"];
 	   Rcpp::NumericMatrix eigvectors = eigdecomp["vectors"];
       // Determine how many evectors to keep
-      double rrankpct;
+      double rrankpct=90;          // rrankpct not coming correctly from the modelterm now
       size_t nColUsed = 0;
-      if(col.hasAttribute("rrankpct"))
-         rrankpct = col.attr("rrankpct");
-      else
-         rrankpct = 99;
       double sumeval = 0.0l;                   // only summing the positive ones!
       for (size_t i = 0; i<eigvalues.size() && eigvalues[i] > 0; i++) sumeval += eigvalues[i];
       double eval_cutoff = rrankpct * sumeval / 100.0l;
@@ -60,6 +56,61 @@ public:
    }
 
    ~kernelMatrix() {
+   }
+
+   // Add a kernel (make the kronecker product) to the stored kernel in the object.
+   void addKernel(kernelMatrix* K2) {
+      // make list (and sort it) of all interaction evalues
+ 	   std::vector<double> evalint(this->ncol*K2->ncol, 0.0l);
+      double sumeval = 0.0l;
+	   for (size_t i = 0; i<this->ncol; i++) {
+		  for (size_t j = 0; j<K2->ncol; j++) {
+           evalint[i*K2->ncol + j] = this->weights[i] * K2->weights[j];
+			  sumeval += evalint[i*K2->ncol + j];
+		  }
+      }
+	   std::sort(evalint.begin(), evalint.end(), std::greater<double>());
+      // Determine cut-off eval to reach rrankpct cumulative sum
+      double rrankpct=90;          // rrankpct not coming correctly from the modelterm now
+	   double eval_sum_cutoff = rrankpct * sumeval / 100.0l;  // this is cut-off on cumulative eval
+	   sumeval = 0.0l;
+	   unsigned long nEvalUsed=0;
+	   while ((sumeval += evalint[nEvalUsed]) < eval_sum_cutoff) nEvalUsed++;
+	   double eval_min_cutoff = evalint[nEvalUsed];  // this is cut-off evalue to get rrankpct cumulative
+      nEvalUsed++;
+      // Set-up a temporary  matrix and vector to make the evectors and evalues for the interaction kernel.
+      // For the moment I keep setting up for all combinations. 
+      size_t nLevel1 = this->nrow;
+      size_t nLevel2 = K2->nrow;
+      simpleMatrix tempEvecs(nLevel1*nLevel2, nEvalUsed);
+      simpleDblVector tempEvals(nEvalUsed);
+      size_t k = 0;
+      for(size_t i=0; i<this->ncol; i++) {
+         for(size_t j=0; j<K2->ncol; j++) {
+            if ( this->weights[i]*K2->weights[j] >= eval_min_cutoff ) {
+               tempEvals[k] = this->weights[i] * K2->weights[j];
+               for(size_t rowi=0; rowi<nLevel1; rowi++) {
+                  for(size_t rowj=0; rowj<nLevel2; rowj++) {
+                     tempEvecs[i*nLevel2+j,k] = this->data(rowi,i) * M2->data(rowj,j);
+                  }
+               }
+               k++;
+            }
+         }
+      }
+      // Make labels for the new combined matrix
+      std::vector<std::string> tempLabels;
+      tempLabels.reserve(nLevel1*nLevel2);
+      for(size_t rowi=0; rowi<nLevel1; rowi++) {
+         for(size_t rowj=0; rowj<nLevel2; rowj++) {
+            tempLabels.push_back(this->rownames[rowi]+"%"+K2->rownames[rowj]);
+         }
+      }
+      // Swap the old data with the new data.
+      // Note the old data is removed when tempEvecs, tempEvals and tempLabels here go out of scope.
+      this->swap(tempEvecs);
+      weights->swap(tempEvals);
+      std::swap(rownames,tempLabels);
    }
 
    std::vector<std::string> rownames, colnames;
