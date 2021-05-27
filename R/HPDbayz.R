7870a830477b4580e6f9cf49b1ec3a6add8189d8
Luc Janss <luc.janss@qgg.au.dk>
2021-05-27 11:05:09 +0200

    Update HPDbayz.R so it does not crash with all identical values input

--- R/HPDbayz.R
+++ R/HPDbayz.R
@@ -5,8 +5,10 @@
 #' @param prob the target probability (0,1) to collect in the HPD interval; as the underlying density estimate
 #'             is discrete, HPDbayz will obtain the interval that includes just more than the given 'prob'
 #' @param bound a single character string or vector of character strings giving the types of bounds on the parameters,
-#'              which must be any of "none", "var" (parameter is restricted to (0,INF)), or "prob" (parameter is
-#'              restricted to (0,1)). 
+#'              which must be either "none", "var" (parameter is restricted to (0,INF)), or "prob" (parameter is
+#'              restricted to (0,1)). When x has multiple columns, a single character string can be given as bound and
+#'              that bound is applied to all columns, or a vector of character strings can be given to indicate the
+#'              bound to be applied to each colunm.
 #'
 #' @return A matrix with 2 columns for the lower and upper values of the HPD interval, and a row for every input column.
 #' @export
@@ -34,16 +36,22 @@ HPDbayz <- function(x, prob=0.95, bound="none") {
            densestim = standarddens
         }
         else {
-           stop("In HPDbayz bound-type is not any of 'none','var' or 'prob'")
+           stop("In HPDbayz bound-type is not 'none','var' or 'prob'")
         }
         n = length(densestim$x)
+        if ( is.null(n) | length(densestim$x != length(densestim$y)) | n<5 ) {
+           # this seems not to occur, density() looks quite robust and always makes a nice grid of
+           # 512 points, but it is possible that the grid has all identical values if the input is
+           # all identical values.
+           stop("Problem in HPDbayz computing the densities for HPD intervals")
+        }
         densestim$y = densestim$y/(sum(densestim$y)-0.5*densestim$y[1]-0.5*densestim$y[n])
         hpdleft = 1
         hpdright = n
         tailprob = 0
         leftbinprob = 0.5*(densestim$y[hpdleft]+densestim$y[hpdleft+1])
         rightbinprob = 0.5*(densestim$y[hpdright]+densestim$y[hpdright-1])
-        while ( (tailprob + min(leftbinprob,rightbinprob) < (1-prob) ) ) {
+        while ( (tailprob + min(leftbinprob,rightbinprob) < (1-prob) ) & (hpdleft < (n-2)) & (hpdright>2) ) {
             if (leftbinprob < rightbinprob) {
                 tailprob = tailprob + leftbinprob
                 hpdleft = hpdleft + 1
