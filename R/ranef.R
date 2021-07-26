3ec05088af5e310c0755e7e1bca139c58e33a53a
Luc Janss <luc.janss@mbg.au.dk>
2021-07-26 11:02:33 +0200

    updated ranef to extract random effects

--- R/ranef.R
+++ R/ranef.R
@@ -4,11 +4,33 @@
 #' called random in mixed models,
 #' i.e. those that have shrinkage priors such as a Normal prior, but also other shrinkage priors.
 #'
-#' @param object  A bayz model output
-#' @splitLabels   Whether labels that contain % (in interactions) such as a%b should be split in multiple columns (default TRUE). 
-#' @param ...     Additional parameters passed onto the Model function.
+#' @param object        A bayz model output
+#' @param splitLabels   Whether labels that contain % (in interactions) such as a%b should be split
+#'                      in multiple columns (default TRUE). 
+#' @param ...           Additional parameters passed onto the Model function.
 #'
 #' @return 
 #' @export
 ranef.bayz <- function(object, splitLabels=TRUE, ...){
+    new_object = list()
+    par = object$Parameters
+    est = object$Estimates
+    par_select = ( par$ModelTerm=="fx" | par$ModelTerm=="rn" )
+    par = par[par_select,]
+    for(i in 1:nrow(par)) {
+        estim_select = est[par$EstStart[i]:par$EstEnd[i],]
+        if(splitLabels) {
+            splitlabels = t(as.data.frame(strsplit(rownames(estim_select),"%")))
+            colnm = splitlabels[1,1]
+            splitlabels = splitlabels[,-1]
+            estim_table = data.frame(splitlabels, estim_select)
+            colnames(estim_table) = c(unlist(strsplit(colnm,":")),colnames(estim_select))
+            rownames(estim_table) = rownames(estim_select)
+        }
+        else {
+            estim_table = estim_select
+        }
+        new_object[[rownames(par)[i]]] = estim_table
+    }
+    return(new_object)
 }
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // mfactor_cpp
 Rcpp::IntegerVector mfactor_cpp(Rcpp::List fact_list);
 RcppExport SEXP _BayzR_mfactor_cpp(SEXP fact_listSEXP) {
