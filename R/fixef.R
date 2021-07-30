c3b1807e7e8c92aec89479fb55685310bc0c9694
Luc Janss <33286944+ljanss@users.noreply.github.com>
2021-07-30 10:45:02 +0200

    Made fixef.R to use a generic coef.R function

---
+++ R/fixef.R
@@ -0,0 +1,16 @@
+#' Extract posterior means and SDs for `fixed´ effects
+#'
+#' Extract from the Bayz output the coefficients (posterior mean and SD) for terms fitted with fx() and rg().
+#' With the default splitLabels=TRUE, labels for interactions such as a%b will be split in multiple columns.
+#'
+#' @param object        A bayz model output
+#' @param splitLabels   Whether labels that contain % (in interactions) such as a%b should be split
+#'                      in multiple columns (default TRUE). 
+#'
+#' @return a list with one member (a data frame) for each fixed effect
+#' @export
+fixef.bayz <- function(object, splitLabels=TRUE){
+    par = object$Parameters
+    par_select = ( par$ModelTerm=="fx" | par$ModelTerm=="rg" )
+    return(coef.bayz(object, par_select, splitLabels)
+}
