89d8dda0f69113555d2b6a2c2a298f0e5dfe5d77
Luc Janss <luc.janss@mbg.au.dk>
2021-08-04 15:39:52 +0200

    Added handling NA in covariate data

--- src/dataCovar.h
+++ src/dataCovar.h
@@ -17,11 +17,31 @@ public:
    
    dataCovar(Rcpp::RObject x) : simpleDblVector() {
       Rcpp::NumericVector temp = Rcpp::as<Rcpp::NumericVector>(x);
+      Rcpp::LogicalVector missing = Rcpp::is_na(temp);
       initWith(temp);
+      size_t countnoNA=0;
+      double sumnoNA=0.0l;
+      for(size_t row=0; row<temp.size(); row++) {
+         if(!missing[row]) {
+            sumnoNA += data[row];
+            countnoNA++;
+         }
+      }
+      offset = sumnoNA / double(countnoNA);
+      for(size_t row=0; row<temp.size(); row++) {
+         if(missing[row])
+            data[row] = 0.0;
+         else
+            data[row] -= offset;
+      }
    }
    
    ~dataCovar() {
    }
+
+   // when fitting a regression on covar, there will be a part offset * regcoeff missing from
+   // the model intercept because that part has been substracted in the centering of the covar.
+   double offset;
    
 };
 
