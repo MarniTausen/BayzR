8b80c7ffe14551b6ecccd6f310b8328952914f3e
Luc Janss <luc.janss@mbg.au.dk>
2021-08-10 11:03:19 +0200

    Added dependency on coda package

--- R/summary.R
+++ R/summary.R
@@ -10,7 +10,7 @@
 #' @param ...       additional parameters
 #'
 #' @return summarybayz object
-#' @import stats
+#' @import stats coda
 #' @export
 summary.bayz <- function(object, maxLevel=10, HPDprob=0.95, ...){
 
@@ -40,26 +40,21 @@ summary.bayz <- function(object, maxLevel=10, HPDprob=0.95, ...){
     output_cycles = as.numeric(rownames(object$Samples))
     if(length(output_cycles<10)) convergence_status = 1       # fail because too few output
     convergence_table = data.frame()
-    if(require("coda")) {
-        samp = coda::mcmc(object$Samples, start=output_cycles[1], end=output_cycles[length(output_cycles)],
-                     thin=output_cycles[2]-output_cycles[1])
-        effSizes = coda::effectiveSize(samp)
-        postMeans =  apply(object$Samples,2,mean)
-        postSDs  = apply(object$Samples,2,sd)
-        MCSEs = postSDs / sqrt(effSizes)
-        MCCVpct = 100 * MCSEs / abs(postMeans)
-        HPDbounds = rep("none",ncol(object$Samples))
-        HPDbounds[substr(colnames(object$Samples),0,3)=="var"] = "var"
-        HPDs = HPDbayz(object$Samples,prob=HPDprob, bound=HPDbounds)
-        GewekeZ = abs(coda::geweke.diag(object$Samples)$z)
-        convergence_table = data.frame(postMeans,postSDs,effSizes,GewekeZ,MCSEs,MCCVpct,HPDs)
-        colnames(convergence_table) = c("postMean","postSD","effSize","GewekeZ","MCSE","MCCV%","HPDleft","HPDright")
-        rownames(convergence_table) = colnames(object$Samples)
-        convergence_status = 0                                # success
-    }
-    else {   # no coda package available
-        convergence_status = 2                                # fail because of missing coda package
-    }
+    samp = coda::mcmc(object$Samples, start=output_cycles[1], end=output_cycles[length(output_cycles)],
+                 thin=output_cycles[2]-output_cycles[1])
+    effSizes = coda::effectiveSize(samp)
+    postMeans =  apply(object$Samples,2,mean)
+    postSDs  = apply(object$Samples,2,sd)
+    MCSEs = postSDs / sqrt(effSizes)
+    MCCVpct = 100 * MCSEs / abs(postMeans)
+    HPDbounds = rep("none",ncol(object$Samples))
+    HPDbounds[substr(colnames(object$Samples),0,3)=="var"] = "var"
+    HPDs = HPDbayz(object$Samples,prob=HPDprob, bound=HPDbounds)
+    GewekeZ = abs(coda::geweke.diag(object$Samples)$z)
+    convergence_table = data.frame(postMeans,postSDs,effSizes,GewekeZ,MCSEs,MCCVpct,HPDs)
+    colnames(convergence_table) = c("postMean","postSD","effSize","GewekeZ","MCSE","MCCV%","HPDleft","HPDright")
+    rownames(convergence_table) = colnames(object$Samples)
+    convergence_status = 0                                # success
     new_object[['Convergence']] = convergence_table
     new_object[['ConvergenceStatus']] = convergence_status
 
